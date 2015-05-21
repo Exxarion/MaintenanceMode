b00239221cdd92281e7230079afa43337173d9ce
Blumieare Exxarion <Apluscomics@live.com>
2015-05-21 19:34:25 -0400

    Update MM.php

--- src/Exxarion/MaintenanceMode/MM.php
+++ src/Exxarion/MaintenanceMode/MM.php
@@ -2,6 +2,57 @@
 
 namespace MaintenanceMode;
 
-//TODO: Everything
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class MaintenanceMode extends PluginBase {
+  
+  
+  public function onEnable() {
+    $this->getLogger()->info(TextFormat::GREEN."Enabled and ready to use!");
+  }
+  
+  public function onDisable () {
+    $this->getLogger()->info(TextFormat::GREEN."Disabled successfully.");
+  }
+  
+   public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        switch ($command->getName()) {
+            case "maintenance":
+                switch (array_shift($args)) {
+                    case "on":
+                        $sender->sendMessage($this->turnOnMM($sender));
+                        return true;
+                    case "off":
+                        $sender->sendMessage($this->turnOffMM($sender));
+                        return true;
+                    default:
+                        $sender->sendMessage(TextFormat::YELLOW."+-------------------+\n".TextFormat::GREEN."MaintenanceMode\n".TextFormat::RED."/maintenance on\n".TextFormat::GREEN."/maintenance off\n".TextFormat::DARK_RED."- Created by Exxarion\n".TextFormat::YELLOW."+-------------------+");
+                        return true;
+                        
+                }
+            default:
+                return false;
+        }
+    }
+    
+    public function turnOnMM($sender) {
+      $sender->sendMessage(TextFormat::RED."[MM] Enabling Maintenance Mode...");
+            foreach($sender->getServer()->getOnlinePlayers() as $everybody) {
+              foreach(!($everybody->isOP()) as $nonOps) {
+              $nonOps->kick("$message");
+              //TODO: Prevent Non-OPS from connecting
+            }
+              
+    }
+    
+    public funtion turnOffMM($sender) {
+      //TODO: Finish this
+
+}
 
 ?>
